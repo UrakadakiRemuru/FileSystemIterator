9527f07a30cf372bf557909fb4505f9abeb3b5a6
UrakadakiRemuru <met-zi@mail.ru>
2023-08-07 21:41:16 +0300

    1. Посмотреть как добавить функцию генератор в класс. 2. Посмотреть вывод файла

--- test.py
+++ test.py
@@ -1,4 +1,21 @@
 from os import walk
+from pathlib import Path
+class FileSystemIterator:
+    def __init__(self, root, only_files, only_dirs, pattern):
+        self.root = root
+        self.only_files = only_files
+        self.only_dirs = only_dirs
+    def __iter__(self):
+        return self
+    def next(self):
+        if not self.__iter__():
+            raise StopIteration
+        current_dir = os.path.join(self.root, self.current_path)
+        if self.is_directory_only:
+            yield current_dir
+        else:
+            for file in os.listdir(current_dir):
+                yield file
 def degenerator(root: str, only_files=False, only_dirs=False, pattern=None) -> str:
     root_txt = ''
     for i in walk(root):
@@ -12,12 +29,17 @@ def degenerator(root: str, only_files=False, only_dirs=False, pattern=None) -> s
                     break
                 for k in val:
                     if pattern is not None and pattern in k:
-                        yield root_txt + '/' + k
+                        yield Path(root_txt + '/' + k)
                     elif pattern is None:
-                        yield root_txt + '/' + k
+                        yield Path(root_txt + '/' + k)
 
-for i in degenerator('C:/users/mdemin/testdir', only_files=True, pattern='dd'):
+for i in degenerator('C:/users/mdemin/testdir'):
     print(i)
 d = degenerator('C:/users/mdemin/testdir')
-next(d)
-next(d)
+marker = True
+try:
+    while marker:
+        print(next(d))
+except StopIteration:
+    marker = False
+    print('Пизда')
